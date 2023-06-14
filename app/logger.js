706dd3f59395933eac41a19c7b3607737a618ef5
Tiago Alves <a83137@alunos.uminho.pt>
2023-06-14 12:45:41 +0100

    Update logger

--- app/logger.js
+++ app/logger.js
@@ -1,9 +1,14 @@
-const winston = require("winston");
+const { format, createLogger, transports } = require("winston");
+const { combine, timestamp, label, printf } = format;
 
-const logger = winston.createLogger({
+const customFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
   level: "debug",
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
+  format: combine(timestamp(), customFormat),
+  transports: [new transports.Console()],
 });
 
 module.exports = logger;
