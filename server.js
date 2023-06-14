daaec98656d6fa965b36f533f68c4db0e5655cf5
Tiago Alves <a83137@alunos.uminho.pt>
2023-06-14 16:40:02 +0100

    Change host

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const logger = require("./app/logger.js");
 const PORT = process.env.PORT || "3000";
-const HOST = "localhost";
+const HOST = "0.0.0.0";
 app.listen(PORT, HOST);
 logger.info(`Running on http://${HOST}:${PORT}`);
